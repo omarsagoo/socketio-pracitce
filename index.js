505e209790e8700629aab84dacae59603c510544
omarsagoo <om.sagoo@gmail.com>
2021-05-12 01:08:16 -0700

    [add] user typing, user connected/disconnected, nicknames, dont send a message to the user that sent it

--- index.js
+++ index.js
@@ -5,12 +5,42 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+connectedMap = {}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    const _id = socket.id
+
+    io.emit('chat message', {msg: "A user has connected", name: "Admin"})
+    
+    socket.on("typing", (name) => {
+        io.emit('typing', name)
+    })
+
+    socket.on('done typing', () => {
+        io.emit('done typing', 'done');
+    })
+
+    socket.on('connected', (name) => {
+        connectedMap[_id] = name
+        io.emit('connected', connectedMap);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('chat message', {msg: `${connectedMap[_id]} has disconnected`, name: "Admin"});
+
+        delete connectedMap[_id]
+        
+        io.emit('connected', connectedMap)
+    })
+
+    socket.on('chat message', (data) => {
+      io.emit('chat message', data);
+    });
 });
 
 server.listen(3000, () => {
